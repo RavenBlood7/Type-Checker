6e3c747832dd194158043bbdb887e5e946f34949
RavenBlood7 <juan.dupreez82@gmail.com>
2017-04-19 21:25:02 +0200

    The type checker has a few comments to explain how things work

---
+++ Task2a-Type Checker/TypeChecker.java
@@ -0,0 +1,22 @@
+
+import parser.InfoTable;
+import parser.TreeNode;
+
+public class TypeChecker
+{
+	public void doTypeChecking(TreeNode node, InfoTable table)
+	{
+		
+	}
+}
+
+///table has the following interface
+//~ public class InfoTable 
+//~ {	
+	//~ public insert(int ID, String snippet, String type)	
+	//~ public getType(int ID)
+	//~ public setType(int ID, String type)
+	//~ public getText(int ID)
+//~ }
+
+///the TreeNode and the TokenNode are also relevant
