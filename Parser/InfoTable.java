e15d5472c0ab5bda4ccfb63ac7b6fa70fa9a5cb2
Keoagile Dinake <kmdinake@gmail.com>
2017-04-27 17:09:32 +0200

    I dunno,, where to go, but i'll go somewhere

--- Parser/InfoTable.java
+++ Parser/InfoTable.java
@@ -1,10 +1,43 @@
 package parser;
+import java.util.ArrayList;
 
-public class InfoTable 
+public class InfoTable
 {
-	
-	public insert(int ID, String snippet, String type)	
-	public getType(int ID)
-	public setType(int ID, String type)
-	public getText(int ID)
+	private ArrayList<TableItem> symbols = new ArrayList<TableItem>();
+
+	public int index(TableItem item){
+		return symbols.indexOf(item);
+	}
+
+	public void insert(int ID, String tokenClass, String snippet){
+		TableItem item = new TableItem(ID, tokenClass, snippet);
+		symbols.add(item);
+	}
+
+	public char getType(int ID){
+		try {
+			TableItem item = symbols.get(ID);
+			if (item == null) {
+				return '\0';
+			} else {
+				return item.type;
+			}
+		} catch (IndexOutOfBoundsException e){
+			System.out.println("Error: Ooops..failed to get the type\nReason: IndexOutOfBoundsException");
+			return '\0';
+		}
+	}
+
+	public void setType(int ID, char type){
+		try {
+			TableItem item = symbols.get(ID);
+			item.type = type;
+		} catch (IndexOutOfBoundsException e){
+			System.out.println("Error: Ooops..failed to set type.\nReason: IndexOutOfBoundsException");
+		}
+	}
+
+	public String getText(int ID){
+		return "";
+	}
 }
